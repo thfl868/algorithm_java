0bdaabc0407083e4c82064b9993defd9b8bcb270
imsori <dlathfl868@naver.com>
2024-07-23 22:38:10 +0900

    dp

--- src/main/java/com/example/algorithm/dp/bj11727_타일링_dp.java
+++ src/main/java/com/example/algorithm/dp/bj11727_타일링_dp.java
@@ -13,23 +13,25 @@ public class bj11727_타일링_dp {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
             int n = sc.nextInt();
-            dp(n);
-        }
-
-
-        static void dp(int n){
             int[] arr = new int[n+1];
 
             arr[1] = 1;
-            arr[2] = 3;
 
-            if(n >=3 ) {
+            if(n >=2 ) { //	안하면 런타임 에러 (ArrayIndexOutOfBounds)남. 1들어왔을땐 arr[2] 불가
+
+                arr[2] = 3;
+
                 for (int i = 3; i <= n; i++) {
                     arr[i] = (arr[i - 1] + arr[i - 2] * 2) % 10007; // arr에 넣을때부터 10007로 나눈 나머지를 넣어야함!!
                 }
             }
 
             System.out.println(arr[n] );
+        }
+
+
+        static void dp(int n){
+
         }
 
 
