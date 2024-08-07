edcd3cbc41ef28a2dfc510a675e05a52cf5b0726
imsori <dlathfl868@naver.com>
2024-08-07 21:35:20 +0900

    최대힙 & 최소힙 PriorityQueue

---
+++ src/main/java/com/example/algorithm/다익스트라/bj1927_최소힙.java
@@ -0,0 +1,26 @@
+package com.example.algorithm.다익스트라;
+import java.io.*;
+import java.util.*;
+
+public class bj1927_최소힙 {
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue pq = new PriorityQueue();
+        int n = Integer.parseInt(br.readLine());
+        for(int i=0; i<n; i++){
+            int node = Integer.parseInt(br.readLine());
+            if(node == 0) {
+                if(pq.isEmpty()) {
+                    System.out.println("0"); //비어있을 경우
+                }
+                else {
+                    System.out.println( pq.poll());
+                }
+            }else{
+                pq.offer(node);
+            }
+
+        }
+
+    }
+}
